eaf7d86b79ec55ddc485a0b9ffd93d24f288bd57
Vladimir Ventura <vladventura@outlook.es>
2020-07-18 13:39:51 -0400

    Some more changes. Mostly added some kind of input to filter by name but it should be throttled, and also we can see all the pokemon on the backend as well

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import Pokemon from "./Pokemon";
+
+const clt = new ApolloClient({
+  uri: "http://localhost:8000/graphql",
+});
 
 function App() {
+  const [name, setName] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={clt}>
+      <div className="container">
+        <form className="center">
+          <div className="App container">
+            <div className="input-field">
+              <label htmlFor="name">Name</label>
+              <input
+                onChange={(event) => setName(event.target.value)}
+                value={name}
+                delay={1000}
+                id="name"
+                autoComplete="off"
+                type="text"
+              ></input>
+            </div>
+          </div>
+        </form>
+        <Pokemon startsWith={name} />
+      </div>
+    </ApolloProvider>
   );
 }
 
