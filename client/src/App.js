8d54fcaf4e1f64c1a3be3b4a4957b862b72d3bb2
Vladimir Ventura <vladventura@outlook.es>
2020-07-18 18:15:05 -0400

    Added a throttled input, and moved the apollo logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,27 @@
 import React, { useState } from "react";
-import { ApolloProvider } from "@apollo/react-hooks";
-import ApolloClient from "apollo-boost";
-import Pokemon from "./Pokemon";
-
-const clt = new ApolloClient({
-  uri: "http://localhost:8000/graphql",
-});
+import PokemonList from "./components/PokemonList";
+import "./components/ThrottledInput";
+import ThrottledInput from "./components/ThrottledInput";
 
 function App() {
   const [name, setName] = useState("");
 
   return (
-    <ApolloProvider client={clt}>
-      <div className="container">
-        <form className="center">
-          <div className="App container">
-            <div className="input-field">
-              <label htmlFor="name">Name</label>
-              <input
-                onChange={(event) => setName(event.target.value)}
-                value={name}
-                delay={1000}
-                id="name"
-                autoComplete="off"
-                type="text"
-              ></input>
-            </div>
+    <div className="container">
+      <form className="center">
+        <div className="App container">
+          <div className="input-field">
+            <label htmlFor="name">Name</label>
+            <ThrottledInput
+              value={name}
+              onChange={(value) => setName(value)}
+              id="name"
+            />
           </div>
-        </form>
-        <Pokemon startsWith={name} />
-      </div>
-    </ApolloProvider>
+        </div>
+      </form>
+      <PokemonList startsWith={name} />
+    </div>
   );
 }
 
