ca1bfd9fd96cbad569d4eeced9d93240e20cc09d
vladventura <vladventura@outlook.es>
2020-07-21 01:42:39 -0400

    Now the cards have been added a text highlight effect, and their color propagates down to the card body. The background of the card body is a sort of mirror to the upper bounds. Naturally, cards with a single type won't experience this to any extent

--- client/src/components/Pokemon.js
+++ client/src/components/Pokemon.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
 import "./Pokemon.css";
 import LazyLoad from "react-lazyload";
 import { COLOR_MAP } from "../constants";
@@ -11,21 +10,22 @@ const Pokemon = (props) => {
     "--color2": props.type2 ? COLOR_MAP[props.type2] : COLOR_MAP[type1],
   };
   return (
-    <Card className="pokemon-card" data-testid="pokemon-card-container">
-      <LazyLoad data-testid="lazyload-img" offset={200} once={true}>
-        <img
-          style={style}
-          className="card-img"
-          as="img"
-          alt="pokemon"
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${childId}.png`}
-        />
-      </LazyLoad>
-      <Card.Body>
-        <Card.Title data-testid="pokemon-name">{name}</Card.Title>
-        <Card.Text data-testid="pokemon-description">{description}</Card.Text>
-      </Card.Body>
-    </Card>
+    <div className="card pokemon-card" data-testid="pokemon-card-container">
+      <div className="pokemon-card-highlight" style={style}>
+          <img
+            className="card-img"
+            alt="pokemon"
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${childId}.png`}
+          />
+        <div className="card-body">
+          <div className="card-title text" data-testid="pokemon-name">
+            {name}
+            </div>
+          <div className="card-text text" data-testid="pokemon-description">{description}</div>
+        </div>
+      </div>
+    </div>
+
   );
 };
 
