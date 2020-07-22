4bd17fa366ac251dac8f9937b6c991fe273807a8
Vladimir Ventura <vladventura@outlook.es>
2020-07-21 22:47:30 -0400

    This is the new main screen component. As of right now the App works just as it did

---
+++ client/src/components/MainScreen/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import ThrottledInput from "../ThrottledInput";
+import PokemonList from "../PokemonList";
+
+const MainScreen = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <div className="container-fluid" align="center">
+      <ThrottledInput
+        value={name}
+        onChange={(value) => setName(value)}
+        id="name"
+      />
+      <PokemonList startsWith={name} />
+    </div>
+  );
+};
+
+export default MainScreen;
