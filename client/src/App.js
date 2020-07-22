9fbc6d121bd3b1d593fce44c8d602f7b0bf9495a
Vladimir Ventura <vladventura@outlook.es>
2020-07-21 22:27:45 -0400

    Removing the Container component from bootstrap. To be honest, I think I can get rid of this package for the most part and just get their css and js from a cdn, right?

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
 import React, { useState } from "react";
 import PokemonList from "./components/PokemonList";
-import Container from "react-bootstrap/Container";
 import "./components/ThrottledInput";
-
-
 import ThrottledInput from "./components/ThrottledInput";
 
 function App() {
   const [name, setName] = useState("");
 
   return (
-    <Container align="center" fluid>
+    <div className="container" align="center" fluid>
       <ThrottledInput
         value={name}
         onChange={(value) => setName(value)}
         id="name"
       />
       <PokemonList startsWith={name} />
-    </Container>
+    </div>
   );
 }
 
