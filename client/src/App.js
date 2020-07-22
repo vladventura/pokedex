a44b62eaa1310f2414c0024ad357c6973dba12ca
Vladimir Ventura <vladventura@outlook.es>
2020-07-21 22:59:46 -0400

    The router is pretty much set up. I also left some clues as to what to do next as well

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,15 @@ import MainScreen from "./components/MainScreen";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <MainScreen />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={MainScreen} />
+        {/* For later. Uncomment this when ready */}
+        {/* <Route path="/pokemon/:id" component={PokemonDetails} /> */}
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
