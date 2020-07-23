d4f4b545b7f2b1217b297ca593ccbcc3d80629df
vladventura <vladventura@outlook.es>
2020-07-23 12:18:58 -0400

    Removing the comments given that we already implemented most of it

--- client/src/components/PokemonDetail/index.js
+++ client/src/components/PokemonDetail/index.js
@@ -1,12 +1,3 @@
-// We will need Apollo here for GQL queries
-// We will get the pokemon from the id from the router
-// We will request to the Django backend for that singular Pokemon with an id
-// We will display a page with the info about the Pokemon
-// For now, we can create a big container that has the image on the left, the name on top of it and the description below it
-// When we have that data we can come back and display the generation it was introduced in and the ones it shows up in,
-// moves, exp rate, egg hatch rate... you name it
-
-
 import React from 'react';
 import { gql } from 'apollo-boost';
 import { useQuery } from 'react-apollo';
