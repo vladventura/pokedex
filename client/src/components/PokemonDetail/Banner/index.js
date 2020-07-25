162cae30242c52585c1c9c2366179d799c06b4eb
Vladimir Ventura <vladventura@outlook.es>
2020-07-25 16:39:40 -0400

    Mostly removing classes we don't need here, like the container class from bootstrap

--- client/src/components/PokemonDetail/Banner/index.js
+++ client/src/components/PokemonDetail/Banner/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import './index.css';
-import { toUpperCase } from '../../utils';
+import React from "react";
+import "./index.css";
+import { toUpperCase } from "../../utils";
 
-
-const Banner = ({ name, pokeId}) => {
-    return (
-        <div className="banner container row">
-            <div className="col icon-id">
-                <img alt="pokemon" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeId}.png`} />
-                <span>No.{pokeId}</span>
-            </div>
-            <div className="col-lg-8 col name-banner">
-                <span>
-                    {toUpperCase(name)}
-                </span>
-            </div>
-        </div>
-    );
+const Banner = ({ name, pokeId }) => {
+  return (
+    <div className="row banner">
+      <div className="col icon-id">
+        <img
+          alt="pokemon"
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeId}.png`}
+        />
+        <span>No.{pokeId}</span>
+      </div>
+      <div className="col name-banner">
+        <span>{toUpperCase(name)}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
